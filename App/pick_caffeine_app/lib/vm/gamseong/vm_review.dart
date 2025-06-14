c6831582113527864ceedf528cbb2d25e383999f
Eunjun1 <ibbuni8486@gmail.com>
2025-06-15 01:43:14 +0900

    HotFix(06.15 01:43)

--- App/pick_caffeine_app/lib/vm/gamseong/vm_review.dart
+++ App/pick_caffeine_app/lib/vm/gamseong/vm_review.dart
@@ -7,35 +7,32 @@ import 'package:http/http.dart' as http;
 class VmReview extends GetxController {
   final baseUrl = "http://127.0.0.1:8000/seong";
 
-
   final myreviews = <Map<String, dynamic>>[].obs;
   final error = ''.obs;
   final box = GetStorage();
   final isLoading = false.obs;
   final review = <String, dynamic>{}.obs;
 
+  Future<void> userreviews() async {
+    final userId = box.read('loginId'); // 저장된 사용자 ID
+    try {
+      final res = await http.get(Uri.parse("$baseUrl/user/reviews/$userId"));
+      final decoded = json.decode(utf8.decode(res.bodyBytes));
+      print("🔍 서버 응답: $decoded");
 
-Future<void> userreviews() async {
-  final userId = box.read('loginId');  // 저장된 사용자 ID
-  try {
-    final res = await http.get(Uri.parse("$baseUrl/user/reviews/$userId"));
-    final decoded = json.decode(utf8.decode(res.bodyBytes));
-    print("🔍 서버 응답: $decoded");
-
-    if (decoded['result'] == 'OK' && decoded['data'].isNotEmpty) {
-      final raw = decoded['data'][0]; // 첫 번째 리뷰만 사용
-      review.value = {
-        'review_num': raw[0],
-        'review_content': raw[1],
-        'review_image': raw[2],
-        'review_date': raw[3],
-        'review_state': raw[4],
-        'store_id': raw[5],
-      };
+      if (decoded['result'] == 'OK' && decoded['data'].isNotEmpty) {
+        final raw = decoded['data'][0]; // 첫 번째 리뷰만 사용
+        review.value = {
+          'review_num': raw[0],
+          'review_content': raw[1],
+          'review_image': raw[2],
+          'review_date': raw[3],
+          'review_state': raw[4],
+          'store_id': raw[5],
+        };
+      }
+    } catch (e) {
+      print("❗ 오류 발생: $e");
     }
-  } catch (e) {
-    print("❗ 오류 발생: $e");
   }
-}
-
 }
